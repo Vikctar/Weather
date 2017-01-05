fb6dbfa8827b5fd938a649aacf5e849b3c7e0a18
vikctar <vikc.steve@gmail.com>
2017-01-05 11:18:18 +0300

    Update fragment layout

--- app/src/main/java/com/vikcandroid/weather/MainActivity.java
+++ app/src/main/java/com/vikcandroid/weather/MainActivity.java
@@ -1,7 +1,12 @@
 package com.vikcandroid.weather;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Called to have the fragment instantiate its user interface view.
+         * This is optional, and non-graphical fragments can return null (which
+         * is the default implementation).  This will be called between
+         * {@link #onCreate(Bundle)} and {@link #onActivityCreated(Bundle)}.
+         * <p>
+         * <p>If you return a View from here, you will later be called in
+         * {@link #onDestroyView} when the view is being released.
+         *
+         * @param inflater           The LayoutInflater object that can be used to inflate
+         *                           any views in the fragment,
+         * @param container          If non-null, this is the parent view that the fragment's
+         *                           UI should be attached to.  The fragment should not add the view itself,
+         *                           but this can be used to generate the LayoutParams of the view.
+         * @param savedInstanceState If non-null, this fragment is being re-constructed
+         *                           from a previous saved state as given here.
+         * @return Return the View for the fragment's UI, or null.
+         */
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
     }
 }
