21419ebe1dbf87ff2f59c6bb411a1054f487f77a
Vikctar <vikc.steve@gmail.com>
2017-02-19 16:28:19 +0300

    Update the UI with new  data from  server.

--- app/src/main/java/com/vikcandroid/weather/ForecastFragment.java
+++ app/src/main/java/com/vikcandroid/weather/ForecastFragment.java
@@ -180,7 +180,7 @@ public class ForecastFragment extends Fragment {
             dayTime = new android.text.format.Time();
 
             String[] resultStr = new String[numDays];
-            for (int i = 0; i < resultStr.length; i++) {
+            for (int i = 0; i < weatherArray.length(); i++) {
                 // For now, using the format "Day, description, hi/low"
                 String day;
                 String description;
@@ -194,7 +194,7 @@ public class ForecastFragment extends Fragment {
                 // "this saturday".
                 long dateTime;
                 // Cheating to convert this to UTC time, which is what we want anyhow
-                dateTime = dayTime.setJulianDay(julianStartDay + 1);
+                dateTime = dayTime.setJulianDay(julianStartDay + i);
 
                 day = getreadableDateString(dateTime);
 
@@ -319,6 +319,16 @@ public class ForecastFragment extends Fragment {
             }
 
             return null;
+        }
+
+        @Override
+        protected void onPostExecute(String[] strings) {
+            if (strings != null) {
+                forecastAdapter.clear();
+                for (String dayForecast : strings) {
+                    forecastAdapter.add(dayForecast);
+                }
+            }
         }
     }
 }
