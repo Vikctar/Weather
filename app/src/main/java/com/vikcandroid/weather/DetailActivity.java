ffd24a0eca6a533b47ec6f491e61ba15300a9db2
Vikctar <vikc.steve@gmail.com>
2017-03-28 14:17:33 +0300

    grrr... why i left this out, no idea

--- app/src/main/java/com/vikcandroid/weather/DetailActivity.java
+++ app/src/main/java/com/vikcandroid/weather/DetailActivity.java
@@ -4,9 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -44,7 +48,11 @@ public class DetailActivity extends AppCompatActivity {
 
     public static class DetailFragment extends Fragment {
 
+        private static final String LOG_TAG = DetailFragment.class.getSimpleName();
+        private String forecastStr;
+
         public DetailFragment() {
+            setHasOptionsMenu(true);
         }
 
         @Nullable
@@ -56,13 +64,43 @@ public class DetailActivity extends AppCompatActivity {
             // The detail Activity called via Intent. Inspect the intent for forecast data.
             Intent intent = getActivity().getIntent();
             if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-                String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+                forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
                 TextView textView = (TextView) rootView.findViewById(R.id.detail_text);
-                textView.append(forecastStr);
+                textView.setText(forecastStr);
 
             }
 
             return rootView;
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            // Inflate the menu; this adds items to the action bar if it is present
+            inflater.inflate(R.menu.detail_fragment, menu);
+
+            // Retrieve the share menu item
+            MenuItem item = menu.findItem(R.id.action_share);
+
+            // Get the provider and hold onto it to set/change the share intent
+            ShareActionProvider shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+            // Attach an intent to this ShareActionProvider. You can  update this at any time,
+            // like when the user selects a new piece of data they might like to share.
+            if (shareActionProvider != null) {
+                shareActionProvider.setShareIntent(createShareForecastIntent());
+            } else {
+                Log.d(LOG_TAG, "Share Action Provider is null");
+            }
+        }
+
+        private Intent createShareForecastIntent() {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT,
+                    forecastStr + "#VIKC'S WEATHER APP");
+
+            return shareIntent;
         }
     }
 }
