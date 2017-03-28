5d65e2296f3ce1eefce3d6d8dfc3058aa6331f10
Vikctar <vikc.steve@gmail.com>
2017-03-28 14:34:18 +0300

    define location contract constants

--- app/src/main/java/com/vikcandroid/weather/data/WeatherContract.java
+++ app/src/main/java/com/vikcandroid/weather/data/WeatherContract.java
@@ -41,6 +41,19 @@ public class WeatherContract {
     public static final class LocationEntry implements BaseColumns {
         public static final String TABLE_NAME = "location";
 
+        // Location setting string is what will be sent to OWM
+        // as the location query
+        public static final String COLUMN_LOCATION_SETTING = "location_setting";
+
+        // Human readable location string, provided by the API. Because for styling
+        // "Nairobi" is more recognizable than 1043
+        public static final String COLUMN_CITY_NAME = "city_name";
+
+        // In order to uniquely pinpoint the location on the map when we launch the
+        // map intent, we store the latitude and longitude as returned by OWM
+        public static final String COLUMN_COORD_LAT = "coord_lat";
+        public static final String COLUMN_COORD_LONG = "coord_long";
+
     }
 
     /* Inner class that defines the table contents of the weather table */
